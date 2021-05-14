518233c901b425d10397690a60fa6ecec6b17cec
Samantha Malone <sammac99@gmail.com>
2021-05-14 08:41:44 -0700

    debug player name validity

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,9 +160,24 @@ var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
   return value;
 };
+//function to set name
+var getPlayerName = function() {
+  var name = "";
 
+  //******************
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  //************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+
+};
+
+// GAME INFORMATION / VARIABLES
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
